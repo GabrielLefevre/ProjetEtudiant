19025d13cebe5172c9fa86705df638e7fd0bb38c
GabrielLefevre <lfv.gabriel@gmail.com>
2015-10-06 13:11:11 +0200

    Création formulaire ajout étudiant
    possibilité d'ajouter un étudiant via un formulaire

--- client/vues/application/js/etudiant.js
+++ client/vues/application/js/etudiant.js
@@ -1,45 +1,25 @@
-/*
-Please consider that the JS part isn't production ready at all, I just code it to show the concept of merging filters and titles together !
-*/
-$(document).ready(function(){
-    $('.filterable .btn-filter').click(function(){
-        var $panel = $(this).parents('.filterable'),
-        $filters = $panel.find('.filters input'),
-        $tbody = $panel.find('.table tbody');
-        if ($filters.prop('disabled') == true) {
-            $filters.prop('disabled', false);
-            $filters.first().focus();
-        } else {
-            $filters.val('').prop('disabled', true);
-            $tbody.find('.no-result').remove();
-            $tbody.find('tr').show();
-        }
-    });
-
-    $('.filterable .filters input').keyup(function(e){
-        /* Ignore tab key */
-        var code = e.keyCode || e.which;
-        if (code == '9') return;
-        /* Useful DOM data and selectors */
-        var $input = $(this),
-        inputContent = $input.val().toLowerCase(),
-        $panel = $input.parents('.filterable'),
-        column = $panel.find('.filters th').index($input.parents('th')),
-        $table = $panel.find('.table'),
-        $rows = $table.find('tbody tr');
-        /* Dirtiest filter function ever ;) */
-        var $filteredRows = $rows.filter(function(){
-            var value = $(this).find('td').eq(column).text().toLowerCase();
-            return value.indexOf(inputContent) === -1;
-        });
-        /* Clean previous no-result if exist */
-        $table.find('tbody .no-result').remove();
-        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
-        $rows.show();
-        $filteredRows.hide();
-        /* Prepend no-result row if all rows are filtered */
-        if ($filteredRows.length === $rows.length) {
-            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
-        }
-    });
+Template.etudiant.events({
+    'submit form': function(e){
+		e.preventDefault();
+		
+		var nom = $("input[name='nom']").val();
+		var prenom = $("input[name='prenom']").val();
+		var groupe = $("select[name='groupe']").val();
+		var mail = $("input[name='mail']").val();
+		var adresse = $("input[name='adresse']").val();
+		var cp = $("input[name='cp']").val();
+		var ville = $("input[name='ville']").val();
+		
+		var etudiant = {
+				nom: nom,
+				prenom: prenom,
+				mail: mail,
+				groupe: groupe,
+				adresse: adresse,
+				cp: cp,
+				ville: ville
+		}
+        
+        Etudiant.insert(etudiant);
+	}
 });

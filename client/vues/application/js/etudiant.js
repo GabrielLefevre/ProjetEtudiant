179dae88c943c34c02c7c1ac47c3bec017e9526e
GabrielLefevre <lfv.gabriel@gmail.com>
2015-10-06 11:18:58 +0200

    Modification de l'affichage des étudiants
    Version améliorée de l'affichage des étudiants

--- client/vues/application/js/etudiant.js
+++ client/vues/application/js/etudiant.js
@@ -1,111 +1,45 @@
-Template.etudiant.helpers({
-	
-	verifNom: function() {
-		var nom=document.getElementById('nom');
-		if (nom.value=="" || nom.length>25 && nom.length<3){
-			nom.style.backgroundColor = "#8B0000";
-			return false;
-		}
-		else {
-			nom.style.backgroundColor = "";
-			return true;
-		}
-	},
-	
-	verifPrenom: function() {
-		var prenom=document.getElementById('prenom');
-		if (prenom.value=="" || prenom.length>25 && prenom.length<3){
-			prenom.style.backgroundColor = "#8B0000";
-			return false;
-		}
-		else {
-			prenom.style.backgroundColor = "";
-			return true;
-		}
-	},
+/*
+Please consider that the JS part isn't production ready at all, I just code it to show the concept of merging filters and titles together !
+*/
+$(document).ready(function(){
+    $('.filterable .btn-filter').click(function(){
+        var $panel = $(this).parents('.filterable'),
+        $filters = $panel.find('.filters input'),
+        $tbody = $panel.find('.table tbody');
+        if ($filters.prop('disabled') == true) {
+            $filters.prop('disabled', false);
+            $filters.first().focus();
+        } else {
+            $filters.val('').prop('disabled', true);
+            $tbody.find('.no-result').remove();
+            $tbody.find('tr').show();
+        }
+    });
 
-	verifMail: function() {
-		var mail=document.getElementById('mail');
-		if (mail.value=="" || mail.length>25 && mail.length<3){
-			mail.style.backgroundColor = "#8B0000";
-			return false;
-		}
-		else {
-			mail.style.backgroundColor = "";
-			return true;
-		}
-	},
-
-	verifAdresse: function() {
-		var adresse=document.getElementById('adresse');
-		if (adresse.value=="" || adresse.length<50){
-			adresse.style.backgroundColor = "#8B0000";
-			return false;
-		}
-		else {
-			adresse.style.backgroundColor = "";
-			return true;
-		}
-	},
-
-	verifGroupe: function() {
-		var groupe=document.getElementById('groupe');
-		if (groupe.value=="" || groupe.length>3){
-			groupe.style.backgroundColor = "#8B0000";
-			return false;
-		}
-		else {
-			groupe.style.backgroundColor = "";
-			return true;
-		}
-	},
-
-	verifCP: function() {
-		var cp = parseInt(document.getElementById('cp').value);
-		if (document.getElementById('cp').value=="" || document.getElementById('cp').value.length != 5) {
-			document.getElementById('cp').style.backgroundColor = "#8B0000";
-			return false;
-		}
-		else {
-			document.getElementById('cp').style.backgroundColor = "";
-			return true;
-		}
-	},
-	
-	verifVille: function() {
-		var ville=document.getElementById('ville');
-		if (ville.value=="" || ville.length<3 && ville.length>25){
-			ville.style.backgroundColor = "#8B0000";
-			return false;
-		}
-		else {
-			ville.style.backgroundColor = "";
-			return true;
-		}
-	},
-
-	verifForm: function() {
-		/*var n=verifNom();
-		var p=verifPrenom();
-		var m=verifMail();
-		var a=verifAdresse();
-		var g=verifGroupe();
-		var c=verifCode();
-		var v=verifVille();
-		if(n && p && m && a && g && c && v){
-			alert("Formulaire OK");*/
-			Etudiant.insert({prenomE: document.getElementById('prenom').value,nomE: document.getElementById('nom').value,mailE: document.getElementById('mail').value,groupeE: document.getElementById('groupe').value,adresseE: document.getElementById('adresse').value,cpE: document.getElementById('cp').value,villeE: document.getElementById('ville').value});
-		/*}
-		else{
-			alert("Veuillez remplir tous les champs");
-		}*/
-	}
-	
-}),
-
-
-Template.etudiant.events({
-	'click .add': function(e) {
-		Etudiant.insert({prenom: document.getElementById('prenom').value,nom: document.getElementById('nom').value,mail: document.getElementById('mail').value,groupe: document.getElementById('groupe').value,adresse: document.getElementById('adresse').value,cp: document.getElementById('cp').value,ville: document.getElementById('ville').value});
-	}
+    $('.filterable .filters input').keyup(function(e){
+        /* Ignore tab key */
+        var code = e.keyCode || e.which;
+        if (code == '9') return;
+        /* Useful DOM data and selectors */
+        var $input = $(this),
+        inputContent = $input.val().toLowerCase(),
+        $panel = $input.parents('.filterable'),
+        column = $panel.find('.filters th').index($input.parents('th')),
+        $table = $panel.find('.table'),
+        $rows = $table.find('tbody tr');
+        /* Dirtiest filter function ever ;) */
+        var $filteredRows = $rows.filter(function(){
+            var value = $(this).find('td').eq(column).text().toLowerCase();
+            return value.indexOf(inputContent) === -1;
+        });
+        /* Clean previous no-result if exist */
+        $table.find('tbody .no-result').remove();
+        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+        $rows.show();
+        $filteredRows.hide();
+        /* Prepend no-result row if all rows are filtered */
+        if ($filteredRows.length === $rows.length) {
+            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
+        }
+    });
 });

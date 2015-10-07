1dd6d4f93edb061f6b69518e758b57e6e88789ac
damiendubus <damien.dubus@19C15.etu.iut-lens.univ-artois.fr>
2015-10-07 14:47:50 +0200

    Collection Retard

---
+++ client/vues/application/js/prof.js
@@ -0,0 +1,33 @@
+Template.prof.events({
+    'submit form': function(e){
+		e.preventDefault();
+		
+		var nom = $("input[name='nom']").val();
+		var prenom = $("input[name='prenom']").val();
+		var mail = $("input[name='mail']").val();
+		var adresse = $("input[name='adresse']").val();
+		var cp = $("input[name='cp']").val();
+		var ville = $("input[name='ville']").val();
+		
+		var prof = {
+			nom: nom,
+			prenom: prenom,
+			mail: mail,
+			adresse: adresse,
+			cp: cp,
+			ville: ville
+		}
+        
+        Profs.insert(prof);
+	},
+	
+	  'click .delete': function(e) {
+    e.preventDefault();
+
+    if (confirm("supprimer le professeur ?")) {
+      var profCourant = this._id;
+      Profs.remove(profCourant);
+      Router.go('/profs');
+    }
+  }
+});

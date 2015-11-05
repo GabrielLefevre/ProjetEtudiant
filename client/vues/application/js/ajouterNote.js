48eab6efdddf1e32ed26d9ee7ac9970d714c2140
GabrielLefevre <lfv.gabriel@gmail.com>
2015-11-05 14:29:24 +0100

    Ajouts des derniers tests sur les note
    vérifie le nombre d'étudiants inscris et le nombre de notes qui vont
    être ajoutées
    Fait la différence entre un ajout et un update

--- client/vues/application/js/ajouterNote.js
+++ client/vues/application/js/ajouterNote.js
@@ -1,23 +1,23 @@
 /*
 TODO
 
-Lire la ligne 1 de maniere diff�rente pour recuperer les infos sur la promo, la matiere, l'ue et le semestre => OK
-Compter le nombre d'etudiant inscris a ce cours => a verifier
-Verifier qu'il y a autant d'ajout que d'�tudiant sinon le faire remarquer
-Faire la difference entre ajout et modificiation de moyenne => a verifier
-Dire les etudiants sans note (apres ajout)
+Lire la ligne 1 de maniere différente pour recuperer les infos sur la promo, la matiere, l'ue et le semestre => OK
+Compter le nombre d'etudiant inscris a ce cours => OK
+Verifier qu'il y a autant d'ajout que d'étudiant sinon le faire remarquer => OK
+Faire la difference entre ajout et modificiation de moyenne => OK
+Dire les etudiants sans note (apres ajout) => a completer
 exemple de fichier:
 
 
- Matiere	promo	UE
- Lefevre	Gabriel	12	N
+ ANDROID	2014-2015	UE41C
+ Lefevre	Gabriel	12	M
  Dubus	Damien	13	O
  Dupont	Alex	16	O
  */
 Template.note.events({
 	'click .add': function(e) {
 		e.preventDefault();
-		// On recup�re le contenu du textarea
+		// On recupère le contenu du textarea
 		var textarea = $("textarea[id='textarea']").val();
 		// On compte le nombre de ligne du textarea
 		var nbr_ligne = 1;
@@ -52,41 +52,43 @@ Template.note.events({
 			alert("etudiant inscris au cour dans la DB : " + nbr_etu_DB);
 			alert ("nbr ligne etu : " + nbr_etu_list);
 			if (nbr_etu_DB<nbr_etu_list) {
+				alert("vous n'avez pas autant d'étudiant inscris à ce cours dans la base de données ! \n" +
+				"nombre d'étudiant inscris dans la BDD : " + nbr_etu_DB  + "\n nombre d'étudiant dans la liste a ajouter : "+ nbr_etu_list);
+                Router.go('/note');
+			}
+			else if ( nbr_etu_DB >= nbr_etu_list) {
+				if (confirm("Vous allez entrer  " + nbr_etu_list + " note d'étudiant pour les " + nbr_etu_DB + " étudiants inscris à ce cours dans la base de données, continuer ?")) {
+					var etu=[];
+					for (var i=1;i<nbr_ligne;i++) {
+						for (var k=0;k<nbr_colonne;k++) {
+							etu[k]=data[j];
+							j++;
+						}
+						// On crée un objet JS avec les donn�es mise dans le tableau etu[]
+						var note = {
+							nom:etu[0],
+							prenom:etu[1],
+							promo:promo,
+							UE:ue,
+							matiere:matiere,
+							note:etu[2]
+						}
+						if (etu[3]=="M") {
+							var noteTmp = Note.findOne({nom:etu[0],prenom:etu[1],promo:promo,UE:ue,matiere:matiere});
+							idTmp = noteTmp._id;
+							Note.update(_id=idTmp,{$set:{note:etu[2]}});
+						}
+						else {
+							Note.insert(note);
+						}
+						// On cherche l'�tudiant a qui on doit attribuer une note et on modifie son document
+						//var etuTmp = Etudiant.findOne({nom: etu[0], prenom: etu[1]});
+						//idTmp = etuTmp._id;
+						//Etudiant.update(_id=idTmp,{$push:{note:note}});
+					} // for i
+				} // if confirm
+			}
 
-			}
-			else if ( nbr_etu_DB > nbr_etu_list) {
-				
-			}
-			if (confirm("Ajouter les notes  ?")) {
-				var etu=[];
-		 	for (var i=1;i<nbr_ligne;i++) {
-				for (var k=0;k<nbr_colonne;k++) {
-					etu[k]=data[j];
-					j++;
-				}
-				// On crée un objet JS avec les donn�es mise dans le tableau etu[]
-				var note = {
-					nom:etu[0],
-					prenom:etu[1],
-					promo:promo,
-					UE:ue,
-					matiere:matiere,
-					note:etu[2]
-				}
-				if (etu[3]=="M") {
-					var noteTmp = Note.findOne({nom:etu[0],prenom:etu[1],promo:promo,UE:ue,matiere:matiere});
-					idTmp = noteTmp._id;
-					Note.update(_id=idTmp,{note:etu[2]});
-				}
-				else {
-					Note.insert(note);
-				}
-				// On cherche l'�tudiant a qui on doit attribuer une note et on modifie son document
-				//var etuTmp = Etudiant.findOne({nom: etu[0], prenom: etu[1]});
-				//idTmp = etuTmp._id;
-				//Etudiant.update(_id=idTmp,{$push:{note:note}});
-			} // for i
-		} // if confirm
 
 	}
 });
